ed3ceb5ea3bbbb42946cae07f8735ec18d0e4c9f
Paul Shertzinger <pshertzi@gmail.com>
2022-02-18 16:55:12 -0500

    license updated

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,59 +1,8 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-let renderLicenseBadge = (license) => {
-  // console.log(license)
-  switch (license) {
-    case 'MIT':
-      return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
-
-    case 'Apache 2.0':
-      return '![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-yellow.svg)';
-
-
-    case 'BSD-3-Clause':
-      return '![License: BSD-3](https://img.shields.io/badge/License-BSD%203--Clause-yellow.svg)';
-
-    case 'GPLv3':
-      return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-yellow.svg)';
-
-    default:
-      return '';
-  }
-}
-
-
-// TODO: Create a function that returns the license link
-let renderLicenseLink = (license) => {
-  if (license !== "none") {
-    return "![LicenseLink](#license)";
-  } else {
-    return '';
-  }
-}
-
-
-// TODO: Create a function that returns the license section of README
-let renderLicenseSection = (license) => {
-  switch (license) {
-    case 'MIT':
-      return 'License \n [MIT](https://choosealicense.com/licenses/mit/)';
-
-    case 'Apache 2.0':
-      return 'License \n [Apache 2.0](https://choosealicense.com/licenses/apache-2.0/)';
-
-    case 'BSD-3-Clause':
-      return 'License \n [BSD-3-Clause](https://choosealicense.com/licenses/bsd-3-clause-clear/)';
-
-    case 'GPLv3':
-      return 'License \n [GPLv3](https://www.gnu.org/licenses/gpl-3.0)'
-    default:
-      return '';
-  }
-}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  ${renderLicenseSection(data.license)}
+  ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)<br />
   ## Description
 
   ${data.description}
@@ -80,7 +29,7 @@ function generateMarkdown(data) {
 
   ## Badges
   This project falls under the license:
-  ${renderLicenseSection(data.license)}
+  ![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)
 
   ##Questions
   *[Github] (https://github.com/${data.github})
